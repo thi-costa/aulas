74044ad2d39cc6b5f93804728b28ee5661a5d399
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-01-21 10:58:12 -0300

    Aula 02 - backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,28 +6,17 @@
 
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./lib/prisma";
+import { appRoutes } from "./routes";
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
-app.register(cors)
+app.register(cors);
+app.register(appRoutes);
 
 /*
  * Métodos HTTP: Get, Post, Put, Patch, Delete
  **/
-
-app.get("/", async () => {
-    const habits = await prisma.habit.findMany({
-        where: {
-            title: {
-                startsWith: "",
-            },
-        },
-    });
-
-    return habits;
-});
 
 app.listen({
     port: 3333,
