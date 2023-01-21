3e7bd33e8875ff9ccfe607628bb0c1f023b11d03
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-01-21 08:53:16 -0300

    Aula 01 - reactJS

---
+++ web/src/App.tsx
@@ -0,0 +1,19 @@
+import "./styles/global.css";
+
+import { Habit } from "./components/Habit";
+
+function App() {
+    return (
+        <div>
+            <Habit completed={3} />
+            <Habit completed={2} />
+            <Habit completed={1} />
+            <Habit completed={4} />
+            <Habit completed={5} />
+            <Habit completed={7} />
+            <Habit completed={6} />
+        </div>
+    );
+}
+
+export default App;
